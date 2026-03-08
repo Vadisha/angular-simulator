1f20054cd8b418c262f93076b368b929204ce850
Vadim <fazvadim@gmail.com>
2026-03-08 15:15:13 +0500

    fixed errors

--- src/app/training.ts
+++ src/app/training.ts
@@ -80,4 +80,4 @@ const users: User[] = [
   },
 ];
 
-const filteredUsers: User[] = users.filter((user: User) => user.name === "Alice");
+const filteredUsers: User[] = users.filter((user: User):boolean  => user.name === "Alice");
